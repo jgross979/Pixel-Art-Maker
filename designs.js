67404de5a1fca7da6e2217b5926b1c2e10b40469
jgross979 <32721542+jgross979@users.noreply.github.com>
2018-01-21 18:30:46 -0500

    Program works with basic functions

---
+++ designs.js
@@ -0,0 +1,55 @@
+let UIController = (function(){
+  let DOMstrings = {colorPicker: '#colorPicker',
+             height: '#input_height',
+             width: '#input_width',
+             pixelCanvas: '#pixel_canvas',
+             submitButton: '#submit_button'
+             }
+  return{
+    getDOM: function(){
+      return{
+        color: $(DOMstrings.colorPicker),
+        height: $(DOMstrings.height),
+        width: $(DOMstrings.width),
+        canvas: $(DOMstrings.pixelCanvas),
+        submit: $(DOMstrings.submitButton)
+      }
+    }
+  };
+})();
+
+
+let controller = (function(UICtrl){
+  //Get DOM elements
+  let DOM = UICtrl.getDOM();
+  //Initial grid
+  makeGrid();
+
+  DOM.submit.on('click', function(){
+    makeGrid();
+  });
+
+  function clearGrid(){
+    let row = $('tr');
+    let col = $('td');
+    row.remove();
+    col.remove();
+  }
+
+  function makeGrid() {
+    clearGrid();
+    for(row=1; row<= DOM.height.val(); row++){
+      DOM.canvas.append('<tr></tr>');
+    }
+    $('tr').each(function(){
+      for(col=1; col<=DOM.width.val();col++){
+        $(this).append('<td></td>')
+      }
+    })
+  }
+//Draw on click with specified color
+  DOM.canvas.on('mouseover', 'td', function(e){
+    $(this).css('background', DOM.color.val());
+  })
+
+})(UIController);
