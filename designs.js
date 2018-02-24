ef8c85b34b693bd82b2a576556f19f3b961b6a20
Jared Gross <jagross@udel.edu>
2018-02-24 16:26:44 -0500

    Background Color Change/Aesthetics

--- designs.js
+++ designs.js
@@ -1,20 +1,31 @@
 $(function(){
   let UIController = (function() {
     let DOMstrings = {
-      colorPicker: "#colorPicker",
+      background: "#backgroundColor",
+      penColorPicker: "#penColorPicker",
       height: "#input_height",
       width: "#input_width",
       pixelCanvas: "#pixel_canvas",
-      submitButton: "#submit_button"
+      submitButton: "#submit_button",
+      container: '#container',
+      title: ".title",
+      settings: ".settings",
+      canvas_container: ".canvas_container"
     };
     return {
       getDOM: function() {
         return {
-          color: $(DOMstrings.colorPicker),
+          background: $(DOMstrings.background),
+          color: $(DOMstrings.penColorPicker),
           height: $(DOMstrings.height),
           width: $(DOMstrings.width),
           canvas: $(DOMstrings.pixelCanvas),
-          submit: $(DOMstrings.submitButton)
+          submit: $(DOMstrings.submitButton),
+          container: $(DOMstrings.container),
+          title: $(DOMstrings.title),
+          settings: $(DOMstrings.settings),
+          canvasContainer: $(DOMstrings.canvasContainer)
+
         };
       }
     };
@@ -34,11 +45,6 @@ $(function(){
         makeGrid();
       }
     });
-    //Changing Border Color
-    $(DOM.color).on("change", function() {
-      console.log(DOM.color.val());
-      $(".border").css({ border: "5px dashed" + DOM.color.val() });
-    });
 
     function clearGrid() {
       let row = $("tr");
@@ -48,15 +54,22 @@ $(function(){
     }
 
     function makeGrid() {
-      clearGrid();
-      for (row = 1; row <= DOM.height.val(); row++) {
-        DOM.canvas.append("<tr></tr>");
-      }
-      $("tr").each(function() {
-        for (col = 1; col <= DOM.width.val(); col++) {
-          $(this).append("<td></td>");
+      if(DOM.width.val() < 61 && DOM.height.val() < 61){
+        clearGrid();
+        for (row = 1; row <= DOM.height.val(); row++) {
+          DOM.canvas.append("<tr></tr>");
         }
-      });
+        $("tr").each(function() {
+          for (col = 1; col <= DOM.width.val(); col++) {
+            $(this).append("<td></td>");
+          }
+        });
+        $('td').css('background', DOM.background.val())
+      }else(
+        alert('Values must be less than 60, please try again!')
+      )
+
+
     }
     //Draw on click with specified color
     let dragging = false;
@@ -74,5 +87,49 @@ $(function(){
       DOM.canvas.on("click", "td", function(e) {
         $(this).css("background", DOM.color.val());
     });
+
+    //Set proper heights
+    $(function(){
+      let containerHeight = DOM.container.height();
+      let titleHeight = DOM.title.height();
+      let canvasHeight = DOM.canvasContainer.height();
+      $(window).css('height', containerHeight)
+      $('.settings').css('height', canvasHeight )
+    })
+
+    let color = hexToRgb(DOM.background.val());
+    let background = `rgb(${color.r}, ${color.g}, ${color.b})`;
+
+    DOM.background.change(function(){
+      $('td').each(function(){
+        if($(this).css('background-color') === background){
+          $(this).css('background-color', DOM.background.val())
+        }
+        })
+        color = hexToRgb(DOM.background.val())
+        background = `rgb(${color.r}, ${color.g}, ${color.b})`;
+
+        return color, background;
+    })
+
+
+    function hexToRgb(hex) {
+      // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+      var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+      hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+          return r + r + g + g + b + b;
+      });
+
+      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      return result ? {
+          r: parseInt(result[1], 16),
+          g: parseInt(result[2], 16),
+          b: parseInt(result[3], 16)
+      } : null;
+    }
+
+
+
+
 })(UIController);
 })
